7f80fdc6b5e54883e4218a2078eb8d321daa0520
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-14 03:01:53 +0300

    update date

--- src/app/datepipe.pipe.ts
+++ src/app/datepipe.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatepipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any):any  {
+ 
+    
+    let nameDate:Date = new Date();
+    let publishDate:any = new Date(nameDate.getFullYear(),nameDate.getMonth(),nameDate.getDate());
+    let setDate  = Math.abs(publishDate-value)
+    const seconds = 86400;
+    let subSeconds = setDate *0.001
+    let countDate = subSeconds/seconds
+              if(countDate>=0.5) {
+                return Math.ceil(countDate) + " " + "days ago"
+            } 
+            else{
+                return 'Just Now'
+            }
+           
+            }
 
 }
